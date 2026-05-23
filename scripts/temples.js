1185fe6f3d6448b78eab13d9b9c1944876aaaf11
mhan-ctrl <mhansen11@byupathway.edu>
2026-05-23 16:13:54 -0600

    Beginning menu changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,3 +2,11 @@ const currentyear = document.querySelector("#currentyear");
 const lastmodified = document.querySelector("#lastmodified")
 currentyear.textContent = new Date().getFullYear()
 lastmodified.textContent = `Last Modified: ${document.lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
