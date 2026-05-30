97272cf60f71732f6b7044a257009d90b09c0470
mhan-ctrl <mhansen11@byupathway.edu>
2026-05-30 17:43:01 -0600

    Changes

--- scripts/place.js
+++ scripts/place.js
@@ -2,3 +2,5 @@ const currentyear = document.querySelector("#currentyear");
 const lastmodified = document.querySelector("#lastmodified")
 currentyear.textContent = new Date().getFullYear()
 lastmodified.textContent = `Last Modified: ${document.lastModified}`;
+
+//The next course will cover how to use third-party APIs to get real-time weather data
